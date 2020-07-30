58ef0abfdaecde9c85ad145b4f79cca7bfdbe0f0
JasonXuDeveloper <jasonxudeveloper@gmail.com>
2020-07-30 17:08:05 +1000

    Enhance Init.cs

---
+++ Project/Assets/Dependencies/ILRuntime/Generated/CLRBindings.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ILRuntime.Runtime.Generated
+{
+    class CLRBindings
+    {
+
+
+        /// <summary>
+        /// Initialize the CLR binding, please invoke this AFTER CLR Redirection registration
+        /// </summary>
+        public static void Initialize(ILRuntime.Runtime.Enviorment.AppDomain app)
+        {
+            System_Type_Binding.Register(app);
+            System_Collections_Generic_Dictionary_2_Type_UIBehaviour_Binding.Register(app);
+            UnityEngine_Component_Binding.Register(app);
+            UnityEngine_Object_Binding.Register(app);
+            UnityEngine_GameObject_Binding.Register(app);
+            System_Action_1_MonoBehaviourAdapter_Binding_Adaptor_Binding.Register(app);
+            System_Runtime_CompilerServices_AsyncVoidMethodBuilder_Binding.Register(app);
+            UnityEngine_MonoBehaviour_Binding.Register(app);
+            UnityEngine_WaitUntil_Binding.Register(app);
+            System_NotSupportedException_Binding.Register(app);
+            UnityEngine_Application_Binding.Register(app);
+            System_Threading_Tasks_Task_Binding.Register(app);
+            System_Runtime_CompilerServices_TaskAwaiter_Binding.Register(app);
+            System_Collections_Generic_List_1_GameObject_Binding.Register(app);
+            UnityEngine_Time_Binding.Register(app);
+            System_String_Binding.Register(app);
+            System_Int32_Binding.Register(app);
+            UnityEngine_UI_Text_Binding.Register(app);
+            JEngine_Core_Log_Binding.Register(app);
+            UnityEngine_Transform_Binding.Register(app);
+            UnityEngine_Resources_Binding.Register(app);
+            UnityEngine_Color_Binding.Register(app);
+            UnityEngine_UI_Graphic_Binding.Register(app);
+            UnityEngine_Vector2_Binding.Register(app);
+            UnityEngine_RectTransform_Binding.Register(app);
+            UnityEngine_UI_Button_Binding.Register(app);
+            UnityEngine_Events_UnityEvent_Binding.Register(app);
+            UnityEngine_Screen_Binding.Register(app);
+            System_Object_Binding.Register(app);
+            System_Action_Binding.Register(app);
+
+            ILRuntime.CLR.TypeSystem.CLRType __clrType = null;
+        }
+
+        /// <summary>
+        /// Release the CLR binding, please invoke this BEFORE ILRuntime Appdomain destroy
+        /// </summary>
+        public static void Shutdown(ILRuntime.Runtime.Enviorment.AppDomain app)
+        {
+        }
+    }
+}
---
+++ Project/Assets/Scripts/Init.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using ILRuntime.Mono.Cecil.Pdb;
+using JEngine.Core;
+using libx;
+using UnityEngine;
+using AppDomain = ILRuntime.Runtime.Enviorment.AppDomain;
+
+public class Init : MonoBehaviour
+{
+    public static Init Instance;
+    AppDomain appdomain;
+    MemoryStream fs;
+
+    [SerializeField]private string Key;
+
+    void Start()
+    {
+        Instance = this;
+        LoadHotFixAssembly();
+        Application.targetFrameRate = 30;
+    }
+
+    void LoadHotFixAssembly()
+    {
+        appdomain = new AppDomain();
+
+        var dllAsset = Assets.LoadAssetAsync("HotUpdateScripts.bytes",typeof(TextAsset));
+        dllAsset.completed += delegate
+        {
+            if (dllAsset.error != null)
+            {
+                Log.PrintError(dllAsset.error);
+                return;
+            }
+
+            var dll = (TextAsset)dllAsset.asset;
+
+            byte[] original = dll.bytes;
+            try
+            {
+                if (!Assets.runtimeMode)
+                {
+                    original = CryptoHelper.AesDecrypt(original, "DevelopmentMode.");
+                }
+                else
+                {
+                    original = CryptoHelper.AesDecrypt(original, Key);
+                }
+            }
+            catch(Exception ex)
+            {
+                Log.PrintError("加载热更DLL失败，可能是解密密码不正确");
+                Log.PrintError("加载热更DLL错误：\n" + ex.Message);
+                return;
+            }
+            
+            fs = new MemoryStream(original);
+            
+            try
+            {
+                appdomain.LoadAssembly(fs, null, new PdbReaderProvider());
+            }
+            catch(Exception e)
+            {
+                Log.PrintError("加载热更DLL失败，请确保HotUpdateResources/Dll里面有HotUpdateScripts.bytes文件，并且Build Bundle后将DLC传入服务器");
+                Log.PrintError("加载热更DLL错误：\n" + e.Message);
+                return;
+            }
+            
+            
+            InitILrt.InitializeILRuntime(appdomain);
+            OnHotFixLoaded();
+        };
+    }
+
+    
+
+    void OnHotFixLoaded()
+    {
+        appdomain.Invoke("HotUpdateScripts.Program", "RunGame", null, null);
+    }
+    
+    private void OnDestroy()
+    {
+        if (fs != null)
+            fs.Close();
+        fs = null;
+    }
+}
