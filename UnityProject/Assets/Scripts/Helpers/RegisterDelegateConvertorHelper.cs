763a2e456d23cc3ff35cb1c22caec130962ee7bf
JasonXuDeveloper - 傑 <jasonxudeveloper@gmail.com>
2021-09-14 17:16:34 +1000

    Update RegisterDelegateConvertorHelper.cs

--- UnityProject/Assets/Scripts/Helpers/RegisterDelegateConvertorHelper.cs
+++ UnityProject/Assets/Scripts/Helpers/RegisterDelegateConvertorHelper.cs
@@ -45,18 +45,18 @@ namespace JEngine.Helper
                 });
             });
 
-            appdomain.DelegateManager.RegisterDelegateConvertor<JEngine.Core.BindableProperty<System.Int64>.onChange_withOld>((act) =>
+            appdomain.DelegateManager.RegisterDelegateConvertor<JEngine.Core.BindableProperty<System.Int64>.onChangeWithOld>((act) =>
             {
-                return new JEngine.Core.BindableProperty<System.Int64>.onChange_withOld((oldVal, newVal) =>
+                return new JEngine.Core.BindableProperty<System.Int64>.onChangeWithOld((oldVal, newVal) =>
                 {
                     ((Action<System.Int64, System.Int64>)act)(oldVal, newVal);
                 });
             });
 
 
-            appdomain.DelegateManager.RegisterDelegateConvertor<JEngine.Core.BindableProperty<System.Object>.onChange_withOld>((act) =>
+            appdomain.DelegateManager.RegisterDelegateConvertor<JEngine.Core.BindableProperty<System.Object>.onChangeWithOld>((act) =>
             {
-                return new JEngine.Core.BindableProperty<System.Object>.onChange_withOld((oldVal, newVal) =>
+                return new JEngine.Core.BindableProperty<System.Object>.onChangeWithOld((oldVal, newVal) =>
                 {
                     ((Action<System.Object, System.Object>)act)(oldVal, newVal);
                 });
