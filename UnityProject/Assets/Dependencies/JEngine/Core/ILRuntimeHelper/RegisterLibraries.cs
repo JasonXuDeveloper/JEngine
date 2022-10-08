074b26fe886669fe2f06764d363f99bd0f274016
JasonXuDeveloper - 傑 <jasonxudeveloper@gmail.com>
2022-10-08 18:20:31 +1100

    update first time open bug

--- UnityProject/Assets/Dependencies/JEngine/Core/ILRuntimeHelper/RegisterLibraries.cs
+++ UnityProject/Assets/Dependencies/JEngine/Core/ILRuntimeHelper/RegisterLibraries.cs
@@ -6,12 +6,14 @@ namespace JEngine.Core
     {
         public void Register(ILRuntime.Runtime.Enviorment.AppDomain appdomain)
         {
+#if INIT_JE
             //Protobuf适配
             ProtoBuf.PType.RegisterILRuntimeCLRRedirection(appdomain);
             //LitJson适配
             LitJson.JsonMapper.RegisterILRuntimeCLRRedirection(appdomain);
             //Nino适配
             Nino.Serialization.ILRuntimeResolver.RegisterILRuntimeClrRedirection(appdomain);
+#endif
         }
     }
 }
